ae9561bc8e15298269afd162751285a25cc17059
Pipatpol Tanavongchinda <pipatpol.t@ku.th>
2015-12-19 15:42:32 +0700

    Add diffExps

---
+++ Diff.java
@@ -0,0 +1,40 @@
+
+public class Diff implements Visitor {
+
+	@Override
+	public Object visit(PlusExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visit(MinusExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visit(TimesExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visit(DivisionExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visit(NumExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visit(VarExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DiffDivisionExp.java
@@ -0,0 +1,22 @@
+
+public class DiffDivisionExp extends Exp {
+
+	@Override
+	public Object accept(Visitor v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int eval() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDatum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
